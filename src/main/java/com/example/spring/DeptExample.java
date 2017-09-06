a66c552ac3c14f84cb9b9baf9e7c6fa870af7456
java <java@bigdata>
2017-09-06 17:25:19 +0900

    1. spring-context library 추가 	beans.xml 	DeptExample => no Framework(legacy) => Control: Client(100%) 	DeptSpringExample => using Framework => Control: Client(5%), Framework(95%) => IoC

---
+++ src/main/java/com/example/spring/DeptExample.java
@@ -0,0 +1,15 @@
+package com.example.spring;
+
+public class DeptExample {
+
+	public static void main(String[] args) {
+		Dept dept = new Dept();
+		dept.setDeptno(10);
+		dept.setDname("개발부");
+		dept.setLoc("서울");
+		
+		System.out.println(dept.getDeptno() + ", " + dept.getDname() + ", " + dept.getLoc());
+		
+	}
+
+}
